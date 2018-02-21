e2612a615754648bd7fed05f938e2328a844422e
Chris White <c38.white@gmail.com>
2018-02-22 01:34:52 +1000

    Change 'sample data' file name to 'sample_name'

--- radar_real_time_scraper.py
+++ radar_real_time_scraper.py
@@ -1,7 +1,7 @@
 # DO ERROR HANDLING OF CONNECTIONS
 # find a way to reduce the computation time on the rainfall colour conversion
-
 from urllib.request import urlopen, Request
+from urllib.error import URLError
 import cv2
 import os
 import numpy as np
@@ -11,16 +11,29 @@ from sqlalchemy import create_engine
 from bs4 import BeautifulSoup
 import re
 from time import sleep
+import io
 
 
-# connect to ftp server and get a list of all available files
-def check_radar_updates():
+def get_latest_radar_time():
 
     # connect to the BOM ftp radar address
     input_url = "ftp://ftp.bom.gov.au/anon/gen/radar//"
     input_url = input_url.replace(" ", "%20")
-    req = Request(input_url)
-    req_html = urlopen(req).read()
+    retry_connect = True
+    wait_time = 5
+    attempts = 1
+    while retry_connect:
+
+        # Error handling for
+        try:
+            req = Request(input_url)
+            req_html = urlopen(req).read()
+            retry_connect = False
+        except URLError:
+            wait_time = min(wait_time * attempts, 60)
+            sleep(wait_time)
+            print('attempt {}, waiting to retry connection...'.format(attempts))
+            attempts += 1
 
     # parse ftp response string
     soup = str(BeautifulSoup(req_html, 'html.parser').contents[0])
@@ -30,6 +43,8 @@ def check_radar_updates():
     file_list = []
     # return text which begins with IDR and ends with png
     match_string = 'IDR(.*)png'
+
+    # get the latest radar time
     for strings in soup:
         # use regex and apply match_string
         file_match = re.search(match_string, strings, re.M | re.I)
@@ -38,10 +53,97 @@ def check_radar_updates():
             # check that the radar file is the either of the types we want
             components = file_match.group().split('.')
             radar_type = components[0][-1:]
+
             if radar_type.isdigit() or radar_type == 'I':  # if it is a wind or rain intensity radar
                 file_list.append(file_match.group())
 
-    print('getting new list of {} files at {}'.format(len(file_list), datetime.datetime.time(
+    # check each file name and see if it already exists in database
+    append_list = []
+    for radar_file in file_list:
+
+        components = radar_file.split('.')
+        radar_timestamp = int(components[2])  # get the datatime of file
+
+        append_list.append([radar_timestamp])
+
+    # sort the files by date, so that the oldest are downloaded first
+    append_list = sorted(append_list)
+    latest_time = append_list[-1:][0][0]
+
+    return latest_time
+
+
+# some fast way to write a pandas DataFrame to PostgreSQL db
+def load_to_postgresql(df_to_db, engine, db_name):
+
+    output = io.StringIO()
+    df_to_db.to_csv(output, sep='\t', header=False, index=False)
+    output.getvalue()
+    output.seek(0)
+
+    connection = engine.raw_connection()
+    cursor = connection.cursor()
+    columns = df_to_db.columns.tolist()
+    cursor.copy_from(output, db_name, null="", columns=columns)
+    connection.commit()
+    cursor.close()
+
+
+# connect to ftp server and get a list of all available files
+def check_radar_updates(start_time_radar, radar_ids):
+
+    # connect to the BOM ftp radar address
+    input_url = "ftp://ftp.bom.gov.au/anon/gen/radar//"
+    input_url = input_url.replace(" ", "%20")
+    retry_connect = True
+    wait_time = 5
+    attempts = 1
+    while retry_connect:
+
+        # Error handling for
+        try:
+            req = Request(input_url)
+            req_html = urlopen(req).read()
+            retry_connect = False
+        except URLError:
+            wait_time = min(wait_time * attempts, 60)
+            sleep(wait_time)
+            print('attempt {}, waiting to retry connection...'.format(attempts))
+            attempts += 1
+
+    # parse ftp response string
+    soup = str(BeautifulSoup(req_html, 'html.parser').contents[0])
+    soup = soup.splitlines()
+
+    # for each line in ftp response, check a transparency image exists
+    file_list = []
+    # return text which begins with IDR and ends with png
+    match_string = 'IDR(.*)png'
+
+    # get the latest radar time
+    for strings in soup:
+        # use regex and apply match_string
+        file_match = re.search(match_string, strings, re.M | re.I)
+        if file_match:
+
+            # check that the radar file is the either of the types we want
+            components = file_match.group().split('.')
+            radar_type = components[0][-1:]
+            radar = components[0][3:5]
+
+            # get the time
+            radar_time = int(components[2])
+
+            # check if the file was created after the start_time
+            if radar_time >= start_time_radar:
+
+                # check if radar is in list of allowed radars
+                if radar in radar_ids:
+
+                    if radar_type.isdigit() or radar_type == 'I':  # if it is a wind or rain intensity radar
+                        file_list.append(file_match.group())
+
+    print('Getting new list of {} files at {}'.format(len(file_list), datetime.datetime.time(
             datetime.datetime.now().replace(second=0, microsecond=0))))
     print()
     return file_list
@@ -53,9 +155,9 @@ def check_for_new_radar(radar_file_list, engine):
     # check each file name and see if it already exists in database
     append_list = []
     for radar_file in radar_file_list:
-        file_df = pd.read_sql_query(r'select * from file_list where file_name = ' + f"'{radar_file}'", con=engine)
         components = radar_file.split('.')
         radar_timestamp = int(components[2])  # get the datatime of file
+        file_df = pd.read_sql_query(r'select * from file_list where file_name = ' + f"'{radar_file}'", con=engine)
 
         # if file doesn't exist in database, then add it to list
         if file_df.empty:
@@ -64,11 +166,15 @@ def check_for_new_radar(radar_file_list, engine):
     # sort the files by date, so that the oldest are downloaded first
     append_list = sorted(append_list, key=lambda x: x[1])
     append_list = [x[0] for x in append_list]
+
+    print('{} to be downloaded...'.format(len(append_list)))
+    print()
+
     return append_list
 
 
 # download files and save them to the database
-def update_radar_db(file_name, rainfall_intensity_df, engine):
+def update_radar_db(file_name, rainfall_intensity_df, engine, indx_value, indx_length):
 
     # get connection to file
     input_url = "ftp://ftp.bom.gov.au/anon/gen/radar//" + file_name
@@ -78,7 +184,7 @@ def update_radar_db(file_name, rainfall_intensity_df, engine):
     try:
         req_html = urlopen(req).read()
     except IOError:
-        print('Connection lost to file')
+        print('Some files no longer exists')
         return ['reset file list']
 
     # read file
@@ -127,18 +233,27 @@ def update_radar_db(file_name, rainfall_intensity_df, engine):
     efficient_radar_df = pd.merge(radar_df, remove_index, left_index=True, right_index=True)
 
     # connect to database and append radar data and file name
-    print('Appending {} rows of {} radar image at {:02}:{:02} on {:02}/{:02}/{}'.format(len(
-        efficient_radar_df), radar_id, radar_hour, radar_minute, radar_day, radar_month, radar_year))
-    efficient_radar_df.to_sql('radar_reflectivity', engine, if_exists='append', index=False)  # append radar data
+    print('{} remaining: Appending {} rows of radar {} captured at {:02}:{:02} on {:02}/{:02}/{}'.format(
+        (indx_length-indx_value-1), len(efficient_radar_df), radar_id, radar_hour, radar_minute,
+        radar_day, radar_month, radar_year))
+
+    load_to_postgresql(efficient_radar_df, engine, 'radar_reflectivity')  # append radar data
+
     file_list_df = pd.DataFrame(columns=["file_name"], data=[[file_name]])
-    file_list_df.to_sql('file_list', engine, if_exists='append', index=False)  # append file name
+    load_to_postgresql(file_list_df, engine, 'file_list')  # append file name
 
     return None
 
 
 def main():
+
+    start_time = get_latest_radar_time()
+
     # connect to database and query if image already exists
     db_engine = create_engine('postgresql://postgres:Postpassword1@localhost:5432/postgres')
+
+    # radar list
+    radar_list = ['17', '77', '63', '78', '23', '08', '40', '03', '71', '04', '69', '28', '50', '66']
 
     # OS agnostic relative file path
     # get the current directory path
@@ -153,15 +268,17 @@ def main():
     while True:
 
         # get the list of radar files for rain intensity and wind
-        list_of_files = check_radar_updates()
+        list_of_files = check_radar_updates(start_time, radar_list)
 
         # get a list of the radar files to download
         update_list = check_for_new_radar(list_of_files, db_engine)
         update = None
 
+        indx_len = len(update_list)
+
         # load any files that haven't been loaded already
-        for file in update_list:
-            update = update_radar_db(file, rainfall_colour_df, db_engine)
+        for indx, file in enumerate(update_list):
+            update = update_radar_db(file, rainfall_colour_df, db_engine, indx, indx_len)
 
             # error handle for missing files
             if update is not None:
